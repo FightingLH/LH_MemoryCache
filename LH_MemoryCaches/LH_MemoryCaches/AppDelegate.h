88e4128f8248dfaebec045c5c6fa1ae468748add
lihuan <976244150@qq.com>
2017-03-20 11:27:12 +0800

    初始化project

---
+++ LH_MemoryCaches/LH_MemoryCaches/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LH_MemoryCaches
+//
+//  Created by lh on 17/3/20.
+//  Copyright © 2017年 lh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
